294841d5d704299d59695f1b57c06efd5bb0691f
jessey <alexei.iljin@gmail.com>
2013-01-06 03:08:16 +0400

    fixed colortail alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,5 @@ alias reload_zsh='source ~/.zshrc'
 
 alias ls='ls -F --color=auto'
 
-[ -f $(which colortail) ] && alias tail="colortail"
+[ -f "$(which colortail)" ] && alias tail="colortail" || echo "colortail is not installed"
 
